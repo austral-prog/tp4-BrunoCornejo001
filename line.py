01755b995ab499961e3c99a909b4e1b88bb1780a
Bruno <bcornejo@mail.austral.edu.ar>
2025-09-05 11:24:58 -0300

    entrega

--- line.py
+++ line.py
@@ -8,16 +8,19 @@ def line():
     print("El coeficiente B de su ecuacion de la recta es:", B )
     print("El coeficiente x1 de su ecuacion de la recta es:", x1)
     print("El coeficiente x2 de su ecuacion de la recta es:", x2)
+    print(" ")
 
     print("Para la siguiente ecuacion:")
-    print( f"Y = {A}X + {B}")
+    print( f"\tY = {A}X + {B}")
+    print(" ")
 
     y1 = A * x1 + B
     y2 = A * x2 + B
 
     print("Dados los siguientes puntos:")
-    print( f"P1 ({x1}, {y1})")
-    print( f"P2 ({x2}, {y2})")
+    print( f"\tP1 ({x1}, {y1})")
+    print( f"\tP2 ({x2}, {y2})")
+    print("")
 
     distancia = ((float(x2) - float(x1))**2 + (y2 - y1)**2)**0.5
 
