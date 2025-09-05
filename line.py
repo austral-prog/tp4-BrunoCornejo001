aeda64e29ada51077445f98f2f42764e7203da32
BrunoCornejo001 <bcornejo@mail.austral.edu.ar>
2025-09-05 12:41:50 -0300

    Update line.py

--- line.py
+++ line.py
@@ -4,10 +4,10 @@ def line():
     x1 = float(input("Ingrese el coeficiente X1: "))
     x2 = float(input("Ingrese el coeficiente X2: "))
 
-    print("El coeficiente A de su ecuación de la recta es:", A )
-    print("El coeficiente B de su ecuación de la recta es:", B )
-    print("El coeficiente X1 de su ecuación de la recta es:", x1)
-    print("El coeficiente X2 de su ecuación de la recta es:", x2)
+    print("El coeficiente A de su ecuación de la recta es:",A)
+    print("El coeficiente B de su ecuación de la recta es:",B)
+    print("El coeficiente X1 de su ecuación de la recta es:",x1)
+    print("El coeficiente X2 de su ecuación de la recta es:",x2)
     print("")
 
     print("Para la siguiente ecuación:")
